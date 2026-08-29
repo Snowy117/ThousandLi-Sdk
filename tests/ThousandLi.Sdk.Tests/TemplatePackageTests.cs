d6bb0f9bfc0c62a92de7bb0061978916bdc33ceb
Snowy117 <snowflies@outlook.com>
2026-08-29 03:38:32 +0000

    chore: bump version to 0.3.0-preview.2

--- tests/ThousandLi.Sdk.Tests/TemplatePackageTests.cs
+++ tests/ThousandLi.Sdk.Tests/TemplatePackageTests.cs
@@ -105,8 +105,8 @@ public sealed class TemplatePackageTests
 
         Assert.Contains("<IsThousandLiPackageArtifact>true</IsThousandLiPackageArtifact>", project);
         Assert.Contains("<IsThousandLiExpertPackageArtifact>true</IsThousandLiExpertPackageArtifact>", project);
-        Assert.Contains("<PackageReference Include=\"ThousandLi.Contracts\" Version=\"0.3.0-preview.1\" />", project);
-        Assert.Contains("<PackageReference Include=\"ThousandLi.ExpertContracts\" Version=\"0.3.0-preview.1\" />", project);
+        Assert.Contains("<PackageReference Include=\"ThousandLi.Contracts\" Version=\"0.3.0-preview.2\" />", project);
+        Assert.Contains("<PackageReference Include=\"ThousandLi.ExpertContracts\" Version=\"0.3.0-preview.2\" />", project);
     }
 
     [Fact]
